75fb8737a0ff187998d72c135eaa6a1298b92401
shubham-saini17 <161924196+shubham-saini17@users.noreply.github.com>
2024-03-02 22:38:06 +0530

    Update script.js

--- QR_Code_Generator/script.js
+++ QR_Code_Generator/script.js
@@ -4,8 +4,11 @@ const button = document.querySelector('#btn');
 const myQR = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="
 const reset = document.querySelector('#reset');
 let QrImage ="" 
+let count = 0;
+
 
 button.addEventListener('click',function(){
+    count++;
     QrImage = myQR + text.value;
 fetch(`${QrImage}`)
 .then((response)=>{
@@ -16,15 +19,17 @@ fetch(`${QrImage}`)
     const imageElement = document.createElement("img");
     imageElement.src = imageUrl;
     const container = document.getElementById("qr-image");
+    if(count<2){
     container.appendChild(imageElement);
-
+    }
     reset.addEventListener('click',function(){
         document.querySelector('#reset').style.animation= 'rotate 1s infinite';
+        count=0;
     setTimeout((text)=>{
         document.querySelector('#input-box').value = "";
         imageElement.remove();
         document.querySelector('#reset').style.animation ="";
-    },2000)
+    },1000)
 })
 
 }).catch(error => console.error(error));
